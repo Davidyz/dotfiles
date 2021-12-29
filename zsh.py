ef4f8665d291b247bab2616972a71a62c9ac5791
Zhe Yu <zcabzyu@ucl.ac.uk>
2021-12-29 14:38:51 +0000

    Bug fixed.

--- zsh.py
+++ zsh.py
@@ -1,14 +1,37 @@
-import os
+import os, shutil
+ZSH_PATH = os.path.expanduser('~/.oh-my-zsh/')
+ZSH_CUSTOM_PATH = os.path.join(ZSH_PATH, 'custom')
+ZSH_CUSTOM = ZSH_CUSTOM_PATH if os.path.isdir(ZSH_CUSTOM_PATH) else None
+ZSH = ZSH_PATH if os.path.isdir(ZSH_PATH) else None
 
-commands = [
-    'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"',
-    """git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions""",
-    """git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting""",
-    """git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k""",
-    """cd ~/.oh-my-zsh/custom/plugins && git clone git@github.com:srijanshetty/docker-zsh.git docker && cd""",
-    """cd $ZSH_CUSTOM/plugins && git clone https://github.com/chrissicool/zsh-256color""",
-    """  git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions"""
-]
+plugins = {
+    "zsh-autosuggestions": "https://github.com/zsh-users/zsh-autosuggestions",
+    "zsh-syntax-highlighting": "https://github.com/zsh-users/zsh-syntax-highlighting.git",
+    "zsh-256color": "https://github.com/chrissicool/zsh-256color",
+    "zsh-completions": "https://github.com/zsh-users/zsh-completions",
+    "docker": "git@github.com:srijanshetty/docker-zsh.git"
+}
 
-for command in commands:
-    os.system(command)
+themes = {
+        "powerlevel10k": "https://github.com/romkatv/powerlevel10k.git"
+        }
+
+if ZSH is None:
+    if shutil.which('zsh'):
+        os.system('sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"')
+    else:
+        print("zsh not found. Exitting.")
+        exit(0)
+
+if isinstance(ZSH_CUSTOM, str):
+    os.chdir(os.path.realpath(os.path.join(ZSH_CUSTOM, 'plugins')))
+    for name in plugins:
+        url = plugins[name]
+        if not name in os.listdir():
+            os.system(f'git clone {url} {name}')
+
+    os.chdir(os.path.realpath(os.path.join(ZSH_CUSTOM, 'themes')))
+    for name in themes:
+        url = themes[name]
+        if not name in os.listdir():
+            os.system(f'git clone {url} {name}')
