89914360ca100205616bb226a0973bb475504337
Zhe Yu <zcabzyu@ucl.ac.uk>
2021-12-29 14:03:31 +0000

    Add plugin installation commands.

--- zsh.py
+++ zsh.py
@@ -5,7 +5,9 @@ commands = [
     """git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions""",
     """git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting""",
     """git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k""",
-    """cd ~/.oh-my-zsh/custom/plugins && git clone git@github.com:srijanshetty/docker-zsh.git docker && cd"""
+    """cd ~/.oh-my-zsh/custom/plugins && git clone git@github.com:srijanshetty/docker-zsh.git docker && cd""",
+    """cd $ZSH_CUSTOM/plugins && git clone https://github.com/chrissicool/zsh-256color""",
+    """  git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions"""
 ]
 
 for command in commands:
