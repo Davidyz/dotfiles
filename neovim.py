3bc1778c46885d84ddcc0d0aa9b8848912fdeaae
Zhe Yu <zcabzyu@ucl.ac.uk>
2022-01-10 22:54:50 +0000

    Add compiling scripts.

--- neovim.py
+++ neovim.py
@@ -3,6 +3,10 @@ import shutil
 import sys
 
 HOME = os.path.expanduser('~')
+CPU_COUNT = os.cpu_count()
+if CPU_COUNT is None:
+    CPU_COUNT = 1
+
 commands = [
     """sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'""",
     "python -m ensurepip",
@@ -26,7 +30,8 @@ elif shutil.which("apt"):
     commands.extend(extra_commands)
 
 def pre():
-    os.makedirs(f'{HOME}/.config/nvim')
+    if not os.path.isdir(f'{HOME}/.config/nvim'):
+        os.makedirs(f'{HOME}/.config/nvim')
     if os.path.isfile(os.path.expanduser(f'{HOME}/.config/nvim/init.vim')) and not os.path.islink(os.path.expanduser(f'{HOME}/.config/nvim/init.vim')):
         os.system(f'rm {HOME}/.config/nvim/init.vim')
     if os.path.isfile(os.path.expanduser(f'{HOME}/.config/nvim/coc-settings.json')) and not os.path.islink(os.path.expanduser(f'{HOME}/.config/nvim/coc-settings.json')):
@@ -40,6 +45,25 @@ def post():
     os.system('env EDITOR=nvim')
 
 if __name__ == '__main__':
+    if shutil.which('nvim') is None:
+        wd = os.getcwd()
+        os.chdir(os.path.expanduser('~'))
+        if 'git' in os.listdir():
+            os.chdir('git')
+        if shutil.which('yay'):
+            os.system('yay -S neovim-nightly-bin')
+        elif shutil.which('apt'):
+            if not os.path.isdir('neovim'):
+                os.system('git clone git@github.com:neovim/neovim.git')
+            os.system('cd neovim && checkout nightly')
+            os.system('sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen')
+            os.system(f'make -j{os.cpu_count()}')
+            os.system('sudo make install')
+        else:
+            exit()
+        os.system('pip install neovim pynvim')
+        os.system('npm install -g neovim')
+        os.chdir(wd)
     if 'pre' in sys.argv:
         pre()
     elif 'post' in sys.argv:
