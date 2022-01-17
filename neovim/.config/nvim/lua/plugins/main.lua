5aea7df517db9c545f68cfa0371fad2fe7486925
Zhe Yu <zcabzyu@ucl.ac.uk>
2022-01-17 15:53:39 +0000

    Automatically re-compile.

--- neovim/.config/nvim/lua/plugins/main.lua
+++ neovim/.config/nvim/lua/plugins/main.lua
@@ -1,4 +1,11 @@
 require('plugins.packer')
 
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 require('plugins.tree_sitter')
 require('plugins.NERDTree')
