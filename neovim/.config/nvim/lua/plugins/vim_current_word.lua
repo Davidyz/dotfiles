8e2bab543035fb39fa969e955c7ff7d2589925ef
Zhe Yu <zcabzyu@ucl.ac.uk>
2022-01-29 13:53:56 +0000

    Highlight current word.

--- neovim/.config/nvim/lua/plugins/vim_current_word.lua
+++ neovim/.config/nvim/lua/plugins/vim_current_word.lua
@@ -1,2 +1,3 @@
 vim.api.nvim_command([[autocmd BufAdd NERD_tree_*,*.json :let b:vim_current_word_disabled_in_this_buffer = 1]])
 vim.api.nvim_command([[highlight CurrentWordTwins guibg=grey guifg=NONE gui=bold]])
+vim.api.nvim_command([[highlight CurrentWord guibg=grey guifg=NONE gui=NONE]])
