33bece16bc5e92e852e9d9caee0d63abf5211d34
Zhe Yu <zcabzyu@ucl.ac.uk>
2022-02-01 12:47:15 +0000

    Remove repeated commands.

--- neovim.py
+++ neovim.py
@@ -22,7 +22,6 @@ elif shutil.which("apt"):
         extra_commands.append("curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -")
         extra_commands.append("echo deb https://dl.yarnpkg.com/debian/ stable main | sudo tee /etc/apt/sources.list.d/yarn.list""")
         extra_commands.append("sudo apt update && sudo apt install yarn -y")
-        extra_commands.append('sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen')
     elif shutil.which('node') is None:
         extra_commands.append("curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -")
         extra_commands.append("sudo apt update && sudo apt install nodejs -y")
