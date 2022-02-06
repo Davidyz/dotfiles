d3890316fde33bc2bd41d3a1cbdf841525c9b959
Zhe Yu <zcabzyu@ucl.ac.uk>
2022-02-06 23:25:25 +0000

    Linted with black.

--- neovim.py
+++ neovim.py
@@ -2,72 +2,89 @@ import os
 import shutil
 import sys
 
-HOME = os.path.expanduser('~')
+HOME = os.path.expanduser("~")
 CPU_COUNT = os.cpu_count()
 if CPU_COUNT is None:
     CPU_COUNT = 1
 
-commands = [
-    "python -m pip install jupytext flake8 black neovim"
-]
+commands = ["python -m pip install jupytext flake8 black neovim"]
 
 if shutil.which("pacman"):
-    if shutil.which('node') is None:
+    if shutil.which("node") is None:
         commands.append("sudo pacman -S nodejs")
-    if shutil.which('yarn') is None:
+    if shutil.which("yarn") is None:
         commands.append("sudo pacman -S yarn")
 elif shutil.which("apt"):
     extra_commands = []
-    if shutil.which('yarn') is None:
-        extra_commands.append("curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -")
-        extra_commands.append("echo deb https://dl.yarnpkg.com/debian/ stable main | sudo tee /etc/apt/sources.list.d/yarn.list""")
+    if shutil.which("yarn") is None:
+        extra_commands.append(
+            "curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -"
+        )
+        extra_commands.append(
+            "echo deb https://dl.yarnpkg.com/debian/ stable main | sudo tee /etc/apt/sources.list.d/yarn.list"
+            ""
+        )
         extra_commands.append("sudo apt update && sudo apt install yarn -y")
-    elif shutil.which('node') is None:
-        extra_commands.append("curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -")
+    elif shutil.which("node") is None:
+        extra_commands.append(
+            "curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -"
+        )
         extra_commands.append("sudo apt update && sudo apt install nodejs -y")
     commands.extend(extra_commands)
 
+
 def pre():
-    if os.path.islink('~/.config/nvim/init.vim'):
-        os.system('rm ~/.config/nvim/init.vim')
-    if not os.path.isdir(f'{HOME}/.config'):
-        os.makedirs(f'{HOME}/.config')
-    if not os.path.isdir(f'{HOME}/.config/nvim'):
-        os.makedirs(f'{HOME}/.config/nvim')
-    if os.path.isfile(os.path.expanduser(f'{HOME}/.config/nvim/init.vim')) and not os.path.islink(os.path.expanduser(f'{HOME}/.config/nvim/init.vim')):
-        os.system(f'rm {HOME}/.config/nvim/init.vim')
-    if os.path.isfile(os.path.expanduser(f'{HOME}/.config/nvim/coc-settings.json')) and not os.path.islink(os.path.expanduser(f'{HOME}/.config/nvim/coc-settings.json')):
-        os.system(f'rm {HOME}/.config/nvim/coc-settings.json')
+    if os.path.islink("~/.config/nvim/init.vim"):
+        os.system("rm ~/.config/nvim/init.vim")
+    if not os.path.isdir(f"{HOME}/.config"):
+        os.makedirs(f"{HOME}/.config")
+    if not os.path.isdir(f"{HOME}/.config/nvim"):
+        os.makedirs(f"{HOME}/.config/nvim")
+    if os.path.isfile(
+        os.path.expanduser(f"{HOME}/.config/nvim/init.vim")
+    ) and not os.path.islink(os.path.expanduser(f"{HOME}/.config/nvim/init.vim")):
+        os.system(f"rm {HOME}/.config/nvim/init.vim")
+    if os.path.isfile(
+        os.path.expanduser(f"{HOME}/.config/nvim/coc-settings.json")
+    ) and not os.path.islink(
+        os.path.expanduser(f"{HOME}/.config/nvim/coc-settings.json")
+    ):
+        os.system(f"rm {HOME}/.config/nvim/coc-settings.json")
 
     for command in commands:
         os.system(command)
 
 
 def post():
-    os.system("nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'")
-    os.system('env EDITOR=nvim')
+    os.system(
+        "nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'"
+    )
+    os.system("env EDITOR=nvim")
 
-if __name__ == '__main__':
-    if shutil.which('nvim') is None:
+
+if __name__ == "__main__":
+    if shutil.which("nvim") is None:
         wd = os.getcwd()
-        os.chdir(os.path.expanduser('~'))
-        if 'git' in os.listdir():
-            os.chdir('git')
-        if shutil.which('yay'):
-            os.system('yay -S neovim-nightly-bin')
-        elif shutil.which('apt'):
-            if not os.path.isdir('neovim'):
-                os.system('git clone git@github.com:neovim/neovim.git')
-            os.system('cd neovim && checkout nightly')
-            os.system('sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen')
-            os.system(f'make -j{os.cpu_count()}')
-            os.system('sudo make install')
+        os.chdir(os.path.expanduser("~"))
+        if "git" in os.listdir():
+            os.chdir("git")
+        if shutil.which("yay"):
+            os.system("yay -S neovim-nightly-bin")
+        elif shutil.which("apt"):
+            if not os.path.isdir("neovim"):
+                os.system("git clone git@github.com:neovim/neovim.git")
+            os.system("cd neovim && checkout nightly")
+            os.system(
+                "sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen"
+            )
+            os.system(f"make -j{os.cpu_count()}")
+            os.system("sudo make install")
         else:
             exit()
-        os.system('pip install neovim pynvim')
-        os.system('npm install -g neovim')
+        os.system("pip install neovim pynvim")
+        os.system("npm install -g neovim")
         os.chdir(wd)
-    if 'pre' in sys.argv:
+    if "pre" in sys.argv:
         pre()
-    elif 'post' in sys.argv:
+    elif "post" in sys.argv:
         post()
--- zsh.py
+++ zsh.py
@@ -3,8 +3,8 @@ import shutil
 import sys
 
 HOME = os.path.expanduser("~")
-ZSH_PATH = os.path.expanduser(f'{HOME}/.oh-my-zsh/')
-ZSH_CUSTOM_PATH = os.path.join(ZSH_PATH, 'custom')
+ZSH_PATH = os.path.expanduser(f"{HOME}/.oh-my-zsh/")
+ZSH_CUSTOM_PATH = os.path.join(ZSH_PATH, "custom")
 ZSH_CUSTOM = ZSH_CUSTOM_PATH if os.path.isdir(ZSH_CUSTOM_PATH) else None
 ZSH = ZSH_PATH if os.path.isdir(ZSH_PATH) else None
 
@@ -15,40 +15,45 @@ plugins = {
     "zsh-completions": "https://github.com/zsh-users/zsh-completions",
     "docker": "https://github.com/srijanshetty/docker-zsh.git",
     "zsh-autopair": "https://github.com/hlissner/zsh-autopair.git",
-    "zsh-autocomplete": "https://github.com/marlonrichert/zsh-autocomplete.git"
+    "zsh-autocomplete": "https://github.com/marlonrichert/zsh-autocomplete.git",
 }
 
-themes = {
-        "powerlevel10k": "https://github.com/romkatv/powerlevel10k.git"
-        }
+themes = {"powerlevel10k": "https://github.com/romkatv/powerlevel10k.git"}
 
 if ZSH is None:
-    if shutil.which('zsh'):
-        os.system('sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"')
+    if shutil.which("zsh"):
+        os.system(
+            'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+        )
     else:
         print("zsh not found. Exitting.")
         exit(0)
 
+
 def pre():
-    if (not os.path.islink(os.path.expanduser(f'{HOME}/.zshrc'))) and os.path.isfile(os.path.expanduser(f'{HOME}/.zshrc')):
-        os.system(f'mv {HOME}/.zshrc {HOME}/.zshrc.old')
+    if (not os.path.islink(os.path.expanduser(f"{HOME}/.zshrc"))) and os.path.isfile(
+        os.path.expanduser(f"{HOME}/.zshrc")
+    ):
+        os.system(f"mv {HOME}/.zshrc {HOME}/.zshrc.old")
+
 
 def post():
     if isinstance(ZSH_CUSTOM, str):
-        os.chdir(os.path.realpath(os.path.join(ZSH_CUSTOM, 'plugins')))
+        os.chdir(os.path.realpath(os.path.join(ZSH_CUSTOM, "plugins")))
         for name in plugins:
             url = plugins[name]
             if not name in os.listdir():
-                os.system(f'git clone {url} {name}')
+                os.system(f"git clone {url} {name}")
 
-        os.chdir(os.path.realpath(os.path.join(ZSH_CUSTOM, 'themes')))
+        os.chdir(os.path.realpath(os.path.join(ZSH_CUSTOM, "themes")))
         for name in themes:
             url = themes[name]
             if not name in os.listdir():
-                os.system(f'git clone {url} {name}')
+                os.system(f"git clone {url} {name}")
 
-if __name__ == '__main__':
-    if 'pre' in sys.argv:
+
+if __name__ == "__main__":
+    if "pre" in sys.argv:
         pre()
-    elif 'post' in sys.argv:
+    elif "post" in sys.argv:
         post()
