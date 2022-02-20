b5db8b4fe61f4e37f19bea42cd012bc0cb8adf01
Zhe Yu <zcabzyu@ucl.ac.uk>
2022-02-20 21:38:33 +0000

    Updates.

--- neovim/.config/nvim/lua/plugins/coc-nvim.lua
+++ neovim/.config/nvim/lua/plugins/coc-nvim.lua
@@ -9,10 +9,12 @@ vim.g.coc_global_extensions = {'coc-pyright',
   'coc-rainbow-fart',
   'coc-marketplace',
   'coc-grammarly',
-  -- 'coc-json',
+  'coc-json',
   'coc-git',
   'coc-ci',
   'coc-docker',
   'coc-lua',
   'coc-phpls'
 }
+
+vim.api.nvim_command("autocmd FileType python let b:coc_root_patterns = ['.git', '.env', 'venv', '.venv', 'setup.cfg', 'setup.py', 'pyproject.toml', 'pyrightconfig.json']")
