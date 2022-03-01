c9773dcd056b5b4a2b2531e39d932e1b6500b7f8
Zhe Yu <zcabzyu@ucl.ac.uk>
2022-03-01 22:30:04 +0000

    Fix bugs in scoping.

--- neovim/.config/nvim/lua/keymaps/coc-nvim.lua
+++ neovim/.config/nvim/lua/keymaps/coc-nvim.lua
@@ -12,7 +12,7 @@ function Show_documentation()
 end
 
 vim.api.nvim_command([[
-function! s:check_back_space() abort
+function! Check_back_space() abort
   let col = col('.') - 1
   return !col || getline('.')[col - 1]  =~# '\s'
 endfunction
@@ -27,5 +27,5 @@ Set_keymap('n', '<leader>f', '<Plug>(coc-fix-current)', false)
 Set_keymap('i', '<cr>', [[pumvisible() ? coc#_select_confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], { silent=true, expr=true, noremap=true })
 Set_keymap('i', '<c-space>', 'coc#refresh()', { silent=true, expr=true, noremap=true })
 
-Set_keymap('i', '<TAB>', [[pumvisible() ? "\<C-n>" : s:check_back_space() ? "\<TAB>" : coc#refresh()]], { silent=true, expr=true, noremap=true })
+Set_keymap('i', '<TAB>', [[pumvisible() ? "\<C-n>" : Check_back_space() ? "\<TAB>" : coc#refresh()]], { silent=true, expr=true, noremap=true })
 Set_keymap('i', '<S-TAB>', [[pumvisible() ? "\<C-p>" : "\<C-h>"]], { silent=true, expr=true })
