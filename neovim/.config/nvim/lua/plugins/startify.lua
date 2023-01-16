0e3688d940a5733f76f042cf25bcf5dcd7b6a8fe
Zhe Yu <zcabzyu@ucl.ac.uk>
2023-01-16 06:43:35 +0000

    Add git modified and untracked to startify.

--- neovim/.config/nvim/lua/plugins/startify.lua
+++ neovim/.config/nvim/lua/plugins/startify.lua
@@ -1,5 +1,21 @@
 vim.api.nvim_create_autocmd("TabNewEntered", { pattern = "*", command = "Startify" })
 
+local gitModified = function()
+  if vim.fn.has("unix") then
+    local files = vim.fn.systemlist("git ls-files -m 2> /dev/null")
+    if files then
+      return vim.fn.map(files, "{'line': v:val, 'path': v:val}")
+    end
+  end
+end
+local gitUntracked = function()
+  if vim.fn.has("unix") then
+    local files = vim.fn.systemlist("git ls-files -o --exclude-standard 2> /dev/null")
+    if files then
+      return vim.fn.map(files, "{'line': v:val, 'path': v:val}")
+    end
+  end
+end
 if vim.fn.exists(":Startify") ~= 0 then
   vim.api.nvim_command([[
 function! StartifyEntryFormat()
@@ -10,6 +26,8 @@ end
 
 vim.g.startify_lists = {
   { ["type"] = "commands", ["header"] = { "   Commands" } },
+  { type = gitModified, header = { "   Git Modified" } },
+  { type = gitUntracked, header = { "   Git Untracked" } },
   {
     ["type"] = "dir",
     ["header"] = { "   Current directory: " },
