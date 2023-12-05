4b5717534aeb52c552c649a0d2db37f4d7afa9a7
Zhe Yu <zcabzyu@ucl.ac.uk>
2023-12-05 20:25:48 +0000

    Fix color scheme.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,7 +5,9 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = "OneHalfDark"
+--config.color_scheme = "OneHalfDark"
+config.colors, _ = wezterm.color.load_scheme("~/.config/wezterm/onedarkpro.toml")
+
 config.font = wezterm.font("CaskaydiaCove NF")
 config.font_size = 13
 
@@ -36,11 +38,11 @@ config.keys = {
   { key = "-", mods = "CTRL", action = action.DecreaseFontSize },
   { key = "0", mods = "CTRL", action = action.ResetFontSize },
   { key = "C", mods = "CTRL|SHIFT", action = action.CopyTo("Clipboard") },
-  { key = "C", mods = "CTRL|SHIFT", action = action.PasteFrom("Clipboard") },
-  { key = "+", mods = "ALT", action = action.ScrollByLine(1) },
+  { key = "V", mods = "CTRL|SHIFT", action = action.PasteFrom("Clipboard") },
+  { key = "=", mods = "ALT", action = action.ScrollByLine(1) },
   { key = "-", mods = "ALT", action = action.ScrollByLine(-1) },
-  { key = "PageUp", mods = "WIN", action = action.ScrollByPage(-1) },
-  { key = "PageDown", mods = "WIN", action = action.ScrollByPage(1) },
+  { key = "_", mods = "ALT|SHIFT", action = action.ScrollByPage(-1) },
+  { key = "+", mods = "ALT|SHIFT", action = action.ScrollByPage(1) },
 }
 wezterm.on("gui-startup", function(cmd)
   local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
