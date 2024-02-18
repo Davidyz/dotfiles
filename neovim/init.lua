8ada4ed5839450a8236b9c6e6fd12b52d6f72af8
Zhe Yu <zcabzyu@ucl.ac.uk>
2024-02-18 03:25:28 +0000

    Use powershell on windows.

--- neovim/init.lua
+++ neovim/init.lua
@@ -5,6 +5,21 @@ if vim.loader ~= nil and type(vim.loader.enable) == "function" then
   vim.loader.enable()
 end
 
+if vim.fn.has("win32") == 1 then
+  local powershell_options = {
+    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
+
+  for option, value in pairs(powershell_options) do
+    vim.opt[option] = value
+  end
+end
+
 if vim.fn.has("unix") ~= 0 then
   if
     vim.fn.executable("python")
