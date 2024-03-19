f685d29ab501c893d10811d68fa93a88c620de5c
Zhe Yu <zcabzyu@ucl.ac.uk>
2024-03-19 23:53:11 +0000

    Add todo comments.

--- neovim/lua/plugins/_lazy.lua
+++ neovim/lua/plugins/_lazy.lua
@@ -624,11 +624,28 @@ M.plugins = {
   -- "~/git/fauxpilot.nvim",
   -- "github/copilot.vim",
   --{ "altermo/nxwm" },
+  {
+    "folke/todo-comments.nvim",
+    config = function()
+      require("todo-comments").setup()
+      vim.keymap.set("n", "]t", function()
+        require("todo-comments").jump_next()
+      end, { desc = "Next todo comment" })
 
+      vim.keymap.set("n", "[t", function()
+        require("todo-comments").jump_prev()
+      end, { desc = "Previous todo comment" })
+    end,
+  },
   {
     "kawre/leetcode.nvim",
 
-    opts = { lang = "python3" },
+    opts = {
+      lang = "python3",
+      injector = {
+        ["python3"] = { before = true },
+      },
+    },
     init = function()
       vim.g.use_alpha = false
     end,
@@ -639,7 +656,7 @@ M.plugins = {
 
       -- optional
       "nvim-treesitter/nvim-treesitter",
-      "rcarriga/nvim-notify",
+      { "rcarriga/nvim-notify", opts = { background = true } },
       "nvim-tree/nvim-web-devicons",
     },
   },
