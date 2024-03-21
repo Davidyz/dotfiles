7083deb6d78327d94300642f3cdd058f8173f338
Zhe Yu <zcabzyu@ucl.ac.uk>
2024-03-21 18:20:47 +0000

    Misc updates.

--- neovim/lua/plugins/_lazy.lua
+++ neovim/lua/plugins/_lazy.lua
@@ -640,12 +640,28 @@ M.plugins = {
   {
     "kawre/leetcode.nvim",
 
-    opts = {
-      lang = "python3",
-      injector = {
-        ["python3"] = { before = true },
-      },
-    },
+    config = function()
+      local lc = require("leetcode")
+      local python_imports = {
+        "# pyright: reportWildcardImportFromLibrary=false",
+        "# pyright: reportUnusedClass=false",
+        "# pyright: reportUnusedFunction=false",
+        "# pyright: reportUnusedVariable=false",
+      }
+      local default_python_imports = require("leetcode.config.imports").python3
+      for i, v in ipairs(default_python_imports) do
+        if string.find(v, "import") ~= nil then
+          default_python_imports[i] = v .. " # noqa: F401"
+        end
+      end
+      vim.list_extend(python_imports, default_python_imports)
+      lc.setup({
+        lang = "python3",
+        injector = {
+          ["python3"] = { before = python_imports },
+        },
+      })
+    end,
     init = function()
       vim.g.use_alpha = false
     end,
