879aa258c58b64e1f4cf136ebf0e89d0bc78e458
Zhe Yu <zhe.yu.20@ucl.ac.uk>
2024-03-26 03:06:09 +0000

    Fixed. Official builds do not support local install.

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the install location
-BIN_DIR="$HOME/.local/"
+BIN_DIR="/usr/"
 [ ! -d "$BIN_DIR" ] && mkdir -p "$BIN_DIR"
 
 # Fetch the latest nightly release URL for Linux (adjust 'linux64' if necessary for your OS)
@@ -11,4 +11,3 @@ RELEASE_URL="https://github.com/neovim/neovim/releases/download/nightly/nvim-lin
 curl -L "$RELEASE_URL" | tar xz -h -C "$BIN_DIR" --strip-components=1
 
 echo 'Neovim nightly installed successfully.'
-echo "Ensure that $HOME/.local/bin is in your PATH to use the nvim command."
