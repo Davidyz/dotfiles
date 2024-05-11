7eb87eefdd6099d8a9b158e74f0dd185e5b40b07
Zhe Yu <zcabzyu@ucl.ac.uk>
2024-05-12 00:33:19 +0100

    Refactor UFO config.

--- neovim/lua/plugins/_lazy.lua
+++ neovim/lua/plugins/_lazy.lua
@@ -342,6 +342,67 @@ M.plugins = {
       },
     },
   },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async", "nvim-treesitter/nvim-treesitter" },
+    cond = utils.no_vscode,
+    init = function()
+      vim.o.foldenable = false
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldcolumn = "0"
+    end,
+    opts = {
+      fold_virt_text_handler = function(virt_text, lnum, end_lnum, width, truncate)
+        local result = {}
+        local _end = end_lnum - 1
+        local final_text =
+          vim.trim(vim.api.nvim_buf_get_text(0, _end, 0, _end, -1, {})[1])
+        local suffix = final_text:format(end_lnum - lnum)
+        local suffix_width = vim.fn.strdisplaywidth(suffix)
+        local target_width = width - suffix_width
+        local cur_width = 0
+        for _, chunk in ipairs(virt_text) do
+          local chunk_text = chunk[1]
+          local chunk_width = vim.fn.strdisplaywidth(chunk_text)
+          if target_width > cur_width + chunk_width then
+            table.insert(result, chunk)
+          else
+            chunk_text = truncate(chunk_text, target_width - cur_width)
+            local hl_group = chunk[2]
+            table.insert(result, { chunk_text, hl_group })
+            chunk_width = vim.fn.strdisplaywidth(chunk_text)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if cur_width + chunk_width < target_width then
+              suffix = suffix .. (" "):rep(target_width - cur_width - chunk_width)
+            end
+            break
+          end
+          cur_width = cur_width + chunk_width
+        end
+        table.insert(result, { " ⋯ ", "NonText" })
+        table.insert(result, { suffix, "TSPunctBracket" })
+        return result
+      end,
+      provider_selector = function(bufnum, filetype, buftype)
+        if vim.bo.bt == "nofile" then
+          return ""
+        end
+        local clients = vim.lsp.get_clients({ { bufnr = bufnum } })
+        for k, v in pairs(clients) do
+          if v.config.name == "null-ls" then
+            -- ignore null-ls
+            clients[k] = nil
+          end
+        end
+        if #clients ~= 0 then
+          return { "treesitter", "lsp" }
+        end
+        return { "treesitter", "indent" }
+      end,
+    },
+    event = "BufEnter",
+  },
 
   -- NOTE: mason
   {
@@ -608,15 +669,7 @@ M.plugins = {
       })
     end,
   },
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = { "kevinhwang91/promise-async", "nvim-treesitter/nvim-treesitter" },
-    cond = utils.no_vscode,
-    config = function()
-      require("plugins._ufo")
-    end,
-    event = "BufEnter",
-  },
+
   {
     "hedyhli/outline.nvim",
     cond = utils.no_vscode,
