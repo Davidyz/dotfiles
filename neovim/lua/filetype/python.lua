1cf791c7f52849827ddc3527b6cca86a3487ee66
Zhe Yu <zcabzyu@ucl.ac.uk>
2024-06-19 17:22:49 +0100

    Remove python file template.

--- neovim/lua/filetype/python.lua
+++ neovim/lua/filetype/python.lua
@@ -1,17 +1,5 @@
 local ft_utils = require("filetype.utils")
 
-vim.api.nvim_create_autocmd("BufNewFile", {
-  pattern = "*.py",
-  callback = function()
-    ft_utils.initTemplate(
-      "python",
-      { "", "if __name__ == '__main__':", "    pass" },
-      function()
-        return vim.g.editting_code_block ~= true
-      end
-    )
-  end,
-})
 vim.api.nvim_create_autocmd(
   "FileType",
   { pattern = "python", command = "setlocal ts=4 sts=4 expandtab autoindent" }
