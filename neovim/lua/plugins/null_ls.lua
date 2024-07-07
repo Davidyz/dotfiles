26bebd2d66c7415e7e4b20f543d27549ec1c03bf
Zhe Yu <zcabzyu@ucl.ac.uk>
2024-07-07 16:49:11 +0100

    Remove isort (replaced by ruff).

--- neovim/lua/plugins/null_ls.lua
+++ neovim/lua/plugins/null_ls.lua
@@ -7,7 +7,6 @@ return function()
   local mason_config = {
     border = "double",
     sources = {
-      null_ls.builtins.formatting.isort,
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.formatting.prettierd,
       null_ls.builtins.code_actions.gitsigns,
