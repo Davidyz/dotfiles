392ca4495a7257fabe847ef2b8604af777aedc5a
Zhe Yu <zcabzyu@ucl.ac.uk>
2024-07-29 15:26:44 +0100

    Add git tag support.

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
+TAG=stable
+[ -z $1 ] || TAG=$1
 # Define the install location
 BIN_DIR="/usr/"
 [ ! -d "$BIN_DIR" ] && mkdir -p "$BIN_DIR"
 
 # Fetch the latest nightly release URL for Linux (adjust 'linux64' if necessary for your OS)
-RELEASE_URL="https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz"
+RELEASE_URL="https://github.com/neovim/neovim/releases/download/$TAG/nvim-linux64.tar.gz"
 
 # Download and extract the release
 curl -L "$RELEASE_URL" | tar xz -h -C "$BIN_DIR" --strip-components=1
 
-echo 'Neovim nightly installed successfully.'
+echo "Neovim $TAG installed successfully."
