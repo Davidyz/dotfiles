a1910c35c9af47edb22b1bdd5b5c16ac4eebd21f
Davidyz <hzjlyz@gmail.com>
2025-01-18 16:48:03 +0000

    Remove empty file.

--- neovim/lua/keymaps/main.lua
+++ neovim/lua/keymaps/main.lua
@@ -3,16 +3,7 @@ local utils = require("_utils")
 local items = {
   "keymaps.global",
 
-  -- "keymaps.copilot",
-  -- "keymaps.dap",
-  -- "keymaps._lsp",
-  -- "keymaps.markdown_preview",
-  -- "keymaps.NERDTree",
-  -- "keymaps.NERDCommenter",
-  -- "keymaps.telescope",
-
   "keymaps.help",
-  "keymaps.python",
   "keymaps.shell",
   "keymaps.c",
 }
