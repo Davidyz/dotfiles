c17f7c363ffbe0c3c294199a97e2a956b3de7abb
Davidyz <hzjlyz@gmail.com>
2025-03-18 18:59:42 +0000

    automatically open dap-ui

--- neovim/lua/plugins/dap.lua
+++ neovim/lua/plugins/dap.lua
@@ -100,3 +100,8 @@ mason_dap.setup({
     end,
   },
 })
+
+dap.listeners.before.attach.dapui_config = dapui.open
+dap.listeners.before.launch.dapui_config = dapui.open
+dap.listeners.before.event_terminated.dapui_config = dapui.close
+dap.listeners.before.event_exited.dapui_config = dapui.close
