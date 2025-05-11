d3c7d5de08c98478a09b9644716ba3eb128eb8ce
Zhe Yu <zcabzyu@ucl.ac.uk>
2025-05-11 14:03:57 +0800

    accepts arbitrary nvim release url.

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-TAG=stable
-[ -z $1 ] || TAG=$1
+URL=https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
+[ -z "$1" ] || URL=$1
 # Define the install location
 BIN_DIR="/usr/"
 [ ! -d "$BIN_DIR" ] && mkdir -p "$BIN_DIR"
 
-# Fetch the latest nightly release URL for Linux (adjust 'linux64' if necessary for your OS)
-RELEASE_URL="https://github.com/neovim/neovim/releases/download/$TAG/nvim-linux64.tar.gz"
-
 # Download and extract the release
-curl -L "$RELEASE_URL" | tar xz -h -C "$BIN_DIR" --strip-components=1
+curl -L "$URL" | tar xz -h -C "$BIN_DIR" --strip-components=1
 
-echo "Neovim $TAG installed successfully."
+echo "Neovim $URL installed successfully."
