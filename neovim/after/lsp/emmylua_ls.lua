02bcf09008d5299b152e0a822b3c147a9dec3bcf
Davidyz <hzjlyz@gmail.com>
2025-07-02 08:42:39 +0800

    emmylua_ls with neovim runtime

---
+++ neovim/after/lsp/emmylua_ls.lua
@@ -0,0 +1,69 @@
+return {
+  capabilities = {
+    textDocument = {
+      formatting = {
+        dynamicRegistration = true,
+      },
+    },
+  },
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+        requirePattern = { "?/init.lua", "?/lua/?.lua'" },
+      },
+      diagnostics = {
+        globals = {
+          "vim",
+          "require",
+          "pcall",
+          "rawset",
+          "rawget",
+          "getmetatable",
+          "setmetatable",
+          "tostring",
+          "tonumber",
+          "type",
+          "print",
+          "pairs",
+          "ipairs",
+          "error",
+          "assert",
+          "select",
+          "next",
+          "coroutine",
+          "table",
+          "math",
+          "string",
+          "io",
+          "os",
+          "debug",
+          "package",
+        },
+        enableUnrecognisedLocals = false,
+        disableWhenNoFileOpen = true,
+        disableCheckFile = {},
+        disable = { "type-not-found", "unnecessary-if" },
+      },
+      codeAction = {
+        insertSpace = true,
+      },
+      strict = {
+        typeCall = true,
+        arrayIndex = true,
+      },
+      workspace = {
+        library = {
+          vim.env.VIMRUNTIME,
+          vim.fn.stdpath("config"),
+          vim.fn.stdpath("data"),
+        },
+        checkThirdParty = false,
+        enableCodeLens = true,
+      },
+      completion = {
+        enable = true,
+      },
+    },
+  },
+}
