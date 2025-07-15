db9319db7950bb612936b597ef0dd2c4f20fce50
Zhe Yu <zcabzyu@ucl.ac.uk>
2025-07-15 12:22:15 +0800

    feat: update dap switchbuf options

--- neovim/lua/plugins/dap.lua
+++ neovim/lua/plugins/dap.lua
@@ -9,7 +9,7 @@ vim.api.nvim_create_autocmd(
   { pattern = "[dap-repl]", callback = require("dap.ext.autocompl").attach }
 )
 
-dap.defaults.fallback.switchbuf = "usevisible,useopen,usetab,uselast"
+dap.defaults.fallback.switchbuf = "useopen,usetab,usevisible,newtab"
 dap.configurations.lua = {
   {
     type = "nlua",
