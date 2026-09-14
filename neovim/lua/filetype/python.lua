0f9893f87d875054efd1bc3d8b8ce936a2f22ef9
Davidyz <hzjlyz@gmail.com>
2026-09-14 18:26:56 +0800

    fix: minor fixes

--- neovim/lua/filetype/python.lua
+++ neovim/lua/filetype/python.lua
@@ -1,7 +1,1 @@
 vim.api.nvim_create_autocmd("FileType", { pattern = "python", command = "setlocal ts=4 sts=4 expandtab autoindent" })
-vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = "*.ipynb",
-  callback = function()
-    vim.bo.filetype = "ipynb,python"
-  end,
-})
